143408762b05a86385e0aa913f075eab26c923ff
Sophia R <sophie.g.rankin@gmail.com>
2022-08-13 13:08:30 -0400

    worked on models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+})
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'catagory_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tags.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+
+})
 
 module.exports = {
   Product,
